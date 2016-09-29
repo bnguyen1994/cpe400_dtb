832349668bfe76c72d4c38177c522c89ea9ea972
tgoffinet171 <tgoffinet@nevada.unr.edu>
2016-09-28 17:23:00 -0700

    Added newline

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,6 @@
 int main(int argc, char **argv)
 {
   // Do stuff bro
-  std::printf( "Hello, guys!" ); // test for CMake compiler
+  std::printf( "Hello, guys!\n" ); // test for CMake compiler
   return EXIT_SUCCESS;
 }
