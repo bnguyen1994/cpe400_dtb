cbe022f67964cfc00fbcb5036175bb6b407849a8
carpathianslaughter <nguyen.brandon@nevada.unr.edu>
2016-10-03 12:48:02 -0700

    Added sim classes

---
+++ src/simulator.cpp
@@ -0,0 +1,29 @@
+// Program Information ////////////////////////////////////////////////////////
+/**
+ * @file simulator.cpp
+ * 
+ * @author Brandon Thai Nguyen
+ *
+ * @brief Implementation file for simulator class
+ * 
+ * @details Implements all member methods of the simulator class
+ *
+ * @version 1.00
+ *          Michael Leverington (13 April 2016)
+ *          Original Code
+ *
+ * @Note Requires simulator.h
+ */
+
+// Precompiler directives /////////////////////////////////////////////////////
+
+#ifndef CLASS_SIMULATOR_CPP
+#define CLASS_SIMULATOR_CPP
+
+// Header files ///////////////////////////////////////////////////////////////
+
+#include "simulator.h"
+
+// Terminating precompiler directives  ////////////////////////////////////////
+
+#endif		// #ifndef CLASS_SIMULATOR_CPP
---
+++ src/simulator.h
@@ -0,0 +1,33 @@
+// Program Information ////////////////////////////////////////////////////////
+/**
+ * @file simulator.h
+ *
+ * @brief Definition file for simulator class
+ * 
+ * @author Brandon Thai Nguyen
+ * 
+ * @details Specifies all member methods of the simulator class
+ *
+ * @version 1.00
+ *          Brandon Thai Nguyen (03 October 2016)
+ *          Original Code
+ *
+ * @Note None
+ */
+
+// Precompiler directives /////////////////////////////////////////////////////
+
+#ifndef CLASS_SIMULATOR_H
+#define CLASS_SIMULATOR_H
+
+// Header files ///////////////////////////////////////////////////////////////
+
+#include <iostream>
+
+using namespace std;
+
+// Class definition  //////////////////////////////////////////////////////////
+
+// Terminating precompiler directives  ////////////////////////////////////////
+
+#endif		// #ifndef CLASS_SIMULATOR_H
