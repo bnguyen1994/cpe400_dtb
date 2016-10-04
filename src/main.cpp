9d6b6844f6d501decc28429be95f1d7a6f8bae56
carpathianslaughter <nguyen.brandon@nevada.unr.edu>
2016-10-03 21:53:54 -0700

    added doxy comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,9 @@
 /**
  * @file Main.cpp
  *
- * @brief Driver program to exersise string and data structures knowledge
+ * @brief Driver program
  * 
- * @details Driver program that loads and reads a configuration file and its
- *          associated meta-data file
+ * @details
  *
  * @version 1.1
  *          Brandon Thai Nguyen (06 September 2016)
@@ -17,9 +16,20 @@
  *
  * @Note 
  */
+// Precompiler directives /////////////////////////////////////////////////////
+
+    // none
+
+// Header Files ///////////////////////////////////////////////////////////////
 
 #include <iostream>
 #include "simulator.h"
+
+// Global Constant Definitioans  //////////////////////////////////////////////
+
+// Function Prototypes ////////////////////////////////////////////////////////
+
+// Main Function Implementation ///////////////////////////////////////////////
 
 int main(int argc, char **argv)
 {
@@ -27,3 +37,7 @@ int main(int argc, char **argv)
   std::printf( "Hello, guys!\n" ); // test for CMake compiler
   return EXIT_SUCCESS;
 }
+
+// Function Implementation ////////////////////////////////////////////////////
+
+    // None
