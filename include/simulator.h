1937b5b93e93a5924ea1d482e5980607f550b0a7
tgoffinet171 <tgoffinet@nevada.unr.edu>
2016-10-19 15:19:48 -0700

    .h moved to include folder

---
+++ include/simulator.h
@@ -0,0 +1,53 @@
+// Program Information ////////////////////////////////////////////////////////
+/**
+ * @file simulator.h
+ *
+ * @brief Definition file for simulator class
+ * 
+ * @author Brandon Thai Nguyen
+ * 
+ * @details Specifies all member methods of the simulator class
+ *
+ * @version 1.0
+ *          Brandon Thai Nguyen (03 October 2016)
+ *          Original Code
+ *
+ * @Note Requires vehicle.h
+ */
+
+// Precompiler directives /////////////////////////////////////////////////////
+
+#ifndef CLASS_SIMULATOR_H
+#define CLASS_SIMULATOR_H
+
+// Header files ///////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include "vehicle.h"
+#include "../src/world.cpp"
+
+// Class definition  //////////////////////////////////////////////////////////
+class Simulator
+{
+public:
+	// Constructors
+	Simulator();
+	~Simulator();
+
+    // Simulator Control
+    void run();
+    char displayMenu();
+    
+	// World Control
+	void initWorld(int sizeX, int sizeY);
+	bool moveVehicle(int xCoorFrom, int yCoorFrom, int xCoorTo, int yCoorTo);
+	void run(int ticks);
+	void displayWorld();
+
+private:
+    // Class Varibles
+	World<Vehicle> world;
+};
+// Terminating precompiler directives  ////////////////////////////////////////
+
+#endif		// #ifndef CLASS_SIMULATOR_H
