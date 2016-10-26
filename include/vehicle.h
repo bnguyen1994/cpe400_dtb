6f2952567c682b2126b6f9359909ca7b633958d0
tgoffinet171 <tgoffinet@nevada.unr.edu>
2016-10-26 00:25:46 -0700

    Added documentation, movement, AI behavior, and collision avoidance

--- include/vehicle.h
+++ include/vehicle.h
@@ -8,7 +8,13 @@
  *
  * @details Specifies all member methods of the vehicle classes
  *
- * @version 1.00
+ * @version 2.00
+ *          Tyler Michael Goffinet (24 October 2016)
+ *          Added getId(), getLocation(), getDirection(), hasPacket(),
+ *          getNextLocation(), move(), setPacket(), protected functions, and
+ *          member variables
+ *
+ *          1.00
  *          Brandon Thai Nguyen (03 October 2016)
  *          Original Code
  *
@@ -41,46 +47,70 @@ class Vehicle
   // Constructors
   Vehicle( int x, int y, int rowNum, int colNum, bool hasPkt = false );
   ~Vehicle();
-  
+
   // Accessors
   virtual char getId() { return 'V'; }
-  void getLocation( int& x, int& y ) { x = xPos; y = yPos; }
-  void getDestination( int& x, int& y ) { x = xDest; y = yDest; }
+  void getLocation( int& x, int& y )
+  {
+    x = xPos;
+    y = yPos;
+  }
+  void getNextLocation( int& x, int& y )
+  {
+    x = xNextPos;
+    y = yNextPos;
+  }
+  void getDestination( int& x, int& y )
+  {
+    x = xDest;
+    y = yDest;
+  }
   VehicleDir getDirection() { return vehicleDir; }
-  bool hasPacket() { return hasPkt; }
-  void getNextLocation( int& x, int& y );
-  
+  bool       hasPacket() { return hasPkt; }
+
   // Modifiers
   void move();
+  void redirect();
   void setPacket( bool holdsPacket ) { hasPkt = holdsPacket; }
 
  protected:
-  int xPos, yPos;
-  int xDest, yDest;
-  int xNextPos, yNextPos;
-  VehicleDir vehicleDir;
-  int rowNum, columnNum;
+  // Member variables
+  int  xPos, yPos;
+  int  xDest, yDest;
+  int  xNextPos, yNextPos;
   bool hasPkt;
-  
-  void calculateNextLocation();
-  virtual void setDestination() = 0;
+
+  unsigned int redirectCounter;
+  VehicleDir   vehicleDir;
+
+  int rowMax, colMax;
+
+  // Member functions
+  void calcNextLocation();
+  bool calcAltDirection();
+
+  void stop();
+  bool planUp();
+  bool planDown();
+  bool planLeft();
+  bool planRight();
+
+  // Pure virtual fuctions
+  virtual void calculateDestination() = 0;
 };
 
-class Taxi: public Vehicle
+class Taxi : public Vehicle
 {
  public:
   // Constructors
   Taxi( int x, int y, int rowMax, int colMax, bool hasPkt = false );
-  ~Taxi();
-  
+
   // Accessors
   char getId() { return 'T'; }
-  
+
   // Modifiers
-  void setDestination();
+  void calculateDestination();
 };
-
-
 
 // Terminating precompiler directives  ////////////////////////////////////////
 
--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -8,7 +8,12 @@
  *
  * @details Implements all member methods of the vehicle classes
  *
- * @version 1.0
+ * @version
+ *          2.0
+ *          Tyler Goffinet (24 October 2016)
+ *          Implemented functions
+ *
+ *          1.0
  *          Brandon Thai Nguyen (03 October 2016)
  *          Original Code
  *
@@ -26,259 +31,323 @@
 
 // Class implementation ///////////////////////////////////////////////////////
 
-Vehicle::Vehicle( int x, int y, int rowNum, int colNum, bool hasPkt )
-    : xPos(x), yPos(y), rowNum(rowNum), columnNum(colNum), hasPkt(hasPkt)
+/**
+ * @brief Constructor for abstract Vehicle class
+ *
+ * @details Sets up class member variables. Destination set to given position.
+ *          recommend calling setDestination() from derived class constructors.
+ *
+ * @pre None
+ *
+ * @post None
+ *
+ * @par Algorithm
+ *      Sets destination to given position
+ *
+ * @exception
+ *
+ * @param [in] x
+ *             X position of vehicle
+ *
+ *        [in] y
+ *             Y position of vehicle
+ *
+ *        [in] rowMax
+ *             Number of rows allowed
+ *
+ *        [in] colNum
+ *             Number of columns allowed
+ *
+ *        [in] hasPkt
+ *             Contains packet
+ *
+ * @note None
+ */
+Vehicle::Vehicle( int x, int y, int rowMax, int columnMax, bool hasPkt )
+    : xPos( x ),
+      yPos( y ),
+      rowMax( rowMax ),
+      colMax( columnMax ),
+      hasPkt( hasPkt )
 {
   xDest = xPos;
   yDest = yPos;
-  
+
   xNextPos = xPos;
   yNextPos = yPos;
-  
+
   vehicleDir = NaN;
+
+  redirectCounter = 0;
 }
 
-Vehicle::~Vehicle() {}
-
+/**
+ * @brief Moves vehicle
+ *
+ * @details Sets current location to next location and recalcuates
+ *          next location
+ *
+ * @pre None
+ *
+ * @post Current location, next location, and direction are updated
+ *
+ * @par Algorithm
+ *      Set current location to next location
+ *      If current location is destination, then change destination
+ *      Calculate new next location and direction
+ *
+ * @exception None
+ *
+ * @param None
+ *
+ * @return None
+ *
+ * @note Warning: Does NOT know about other vehicles
+ */
 void Vehicle::move()
 {
   xPos = xNextPos;
   yPos = yNextPos;
-  
-  // If destination reached, set new destination
-  if( xPos == xDest && yPos == yDest ) setDestination();
-  
+
+  redirectCounter = 0;
+
+  // If destination reached, calculate new destination
+  if( xPos == xDest && yPos == yDest ) calculateDestination();
+
+  // DEBUG:
+  int        xTemp = xNextPos, yTemp = yNextPos;
+  VehicleDir dirTemp = vehicleDir;
+  calcNextLocation();
+  if( xNextPos < 0 || colMax <= xNextPos || yNextPos < 0 || rowMax <= yNextPos )
+  {
+    xNextPos = 0;
+  }
+  xNextPos   = xTemp;
+  yNextPos   = yTemp;
+  vehicleDir = dirTemp;
+
   // Recalculate next location
-  calculateNextLocation();
+  calcNextLocation();
 }
 
-void Vehicle::getNextLocation( int& x, int& y )
+/**
+ * @brief Redirects the vehicle
+ *
+ * @details Attempts to have the car to take an alternative route
+ *
+ * @pre None
+ *
+ * @post Vehicle direction and next position is modified
+ *
+ * @par Algorithm
+ *      Stage 1: Find route alternative
+ *      Stage 2: Attempt to wiggle your way out
+ *      Stage 3: Give up all hope
+ *
+ * @exception None
+ *
+ * @param None
+ *
+ * @return None
+ *
+ * @note Calling multiple times while not calling move will invoke
+ *       different behaviors
+ */
+void Vehicle::redirect()
 {
-  x = xNextPos;
-  y = yNextPos;
+  ++redirectCounter;
+
+  if( redirectCounter == 1 )
+  {
+    calcAltDirection();
+  }
+  // Attempt to 'wiggle' your way out; if more than 3 trys, give up
+  else if( redirectCounter == 2 )
+  {
+    // Invert direction
+    switch( vehicleDir )
+    {
+      case NaN:
+        vehicleDir = NaN;
+        break;
+      case UP:
+        planDown();
+        break;
+      case DOWN:
+        planUp();
+        break;
+      case LEFT:
+        planRight();
+        break;
+      case RIGHT:
+        planLeft();
+        break;
+    }
+  }
+  else if( redirectCounter == 3 )
+  {
+    // Try a different direction
+    switch( vehicleDir )
+    {
+      case NaN:
+        calcAltDirection();
+        break;
+      case UP:
+        planLeft();
+        break;
+      case DOWN:
+        planRight();
+        break;
+      case LEFT:
+        planUp();
+        break;
+      case RIGHT:
+        planDown();
+        break;
+    }
+  }
+  else if( redirectCounter == 4 )
+  {
+    // Try an even different direction
+    switch( vehicleDir )
+    {
+      case NaN:
+        calcAltDirection();
+        break;
+      case UP:
+        planRight();
+        break;
+      case DOWN:
+        planLeft();
+        break;
+      case LEFT:
+        planDown();
+        break;
+      case RIGHT:
+        planUp();
+        break;
+    }
+  }
+
+  if( redirectCounter == 1 )
+  {
+    // Do nothing
+  }
+  else if( 1 < redirectCounter && redirectCounter < 5 )
+  {
+    // Recalculate with new direction
+    calcNextLocation();
+  }
+  else
+  {
+    // Give up :(
+    vehicleDir = NaN;
+  }
 }
 
-void Vehicle::calculateNextLocation()
+/**
+ * @brief Calculates vehicles's next location
+ *
+ * @details Using current location, next location, and direction,
+ *          recalculates the location the car should be next
+ *
+ * @pre None
+ *
+ * @post None
+ *
+ * @par Algorithm
+ *      Based on the direction, move towards destination along x axis first
+ *      If wanted direction is behind, then make alternate turn
+ *      If direction set to NaN, then any turn is possible
+ *
+ * @exception None
+ *
+ * @param None
+ *
+ * @return None
+ *
+ * @note Does not allow immediate U-turns. If no other options are
+ *       available two move() calls are required to complete a U-turn.
+ *       If direction set to NaN, then any turn is possible
+ */
+void Vehicle::calcNextLocation()
 {
+  xNextPos = xPos;
+  yNextPos = yPos;
+
   // Get front facing locations
   switch( vehicleDir )
   {
     case NaN:
       if( xPos != xDest )
       {
-        if( xPos < xDest )
-        {
-          vehicleDir = RIGHT;
-          ++xNextPos;
-        }
-        else
-        {
-          vehicleDir = LEFT;
-          --xNextPos;
-        }
+        ( xDest < xPos ) ? planLeft() : planRight();
       }
       else if( yPos != yDest )
       {
-        if( yPos < yDest )
-        {
-          vehicleDir = DOWN;
-          ++yNextPos;
-        }
-        else
-        {
-          vehicleDir = UP;
-          --yNextPos;
-        }
+        ( yDest < yPos ) ? planUp() : planDown();
       }
-      else  /* xPos == xDest && yPos == yDest */
+      else /* xPos == xDest && yPos == yDest */
       {
         vehicleDir = NaN;
       }
       break;
-      
+
     case UP:
       if( xPos != xDest )
       {
-        if( xPos < xDest )
-        {
-          vehicleDir = RIGHT;
-          ++xNextPos;
-        }
-        else
-        {
-          vehicleDir = LEFT;
-          --xNextPos;
-        }
+        ( xDest < xPos ) ? planLeft() : planRight();
       }
       else if( yPos != yDest )
       {
-        if( yPos < yDest )
-        {
-          vehicleDir = DOWN;
-          ++yNextPos;
-        }
-        else
-        {
-          // Want to go down, but can't make U-turn
-          if( xPos < xDest && ( xPos + 1 ) < columnNum )
-          {
-            vehicleDir = RIGHT;
-            ++xNextPos;
-          }
-          else if( xPos >= xDest && ( xPos - 1 ) >= 0 )
-          {
-            vehicleDir = LEFT;
-            --xNextPos;
-          }
-          else  /* ( yPos + 1 ) > rowNum && ( yPos - 1 ) < 0 */
-          {
-            // Stop
-            vehicleDir = NaN;
-          }
-        }
+        ( yDest < yPos ) ? planUp() : calcAltDirection();
       }
-      else  /* xPos == xDest && yPos == yDest */
+      else /* xPos == xDest && yPos == yDest */
       {
         vehicleDir = NaN;
       }
       break;
-    
+
     case DOWN:
       if( xPos != xDest )
       {
-        if( xPos < xDest )
-        {
-          vehicleDir = RIGHT;
-          ++xNextPos;
-        }
-        else
-        {
-          vehicleDir = LEFT;
-          --xNextPos;
-        }
+        ( xDest < xPos ) ? planLeft() : planRight();
       }
       else if( yPos != yDest )
       {
-        if( yPos < yDest )
-        {
-          vehicleDir = DOWN;
-          ++yNextPos;
-        }
-        else
-        {
-          // Want to go up, but can't make U-turn
-          if( xPos < xDest && ( xPos + 1 ) < columnNum )
-          {
-            vehicleDir = RIGHT;
-            ++xNextPos;
-          }
-          else if( xPos >= xDest && ( xPos - 1 ) >= 0 )
-          {
-            vehicleDir = LEFT;
-            --xNextPos;
-          }
-          else  /* ( yPos + 1 ) > rowNum && ( yPos - 1 ) < 0 */
-          {
-            // Stop
-            vehicleDir = NaN;
-          }
-        }
+        ( yDest < yPos ) ? calcAltDirection() : planDown();
       }
-      else  /* xPos == xDest && yPos == yDest */
+      else /* xPos == xDest && yPos == yDest */
       {
         vehicleDir = NaN;
       }
       break;
-      
+
     case LEFT:
       if( xPos != xDest )
       {
-        if( xPos < xDest )
-        {
-          // Want to go right, but can't make U-turn
-          if( yPos < yDest && ( yPos + 1 ) < rowNum )
-          {
-            vehicleDir = DOWN;
-            ++yNextPos;
-          }
-          else if( yPos >= yDest && ( yPos - 1 ) >= 0 )
-          {
-            vehicleDir = UP;
-            --yNextPos;
-          }
-          else  /* ( yPos + 1 ) > rowNum && ( yPos - 1 ) < 0 */
-          {
-            // Stop
-            vehicleDir = NaN;
-          }
-        }
-        else
-        {
-          vehicleDir = LEFT;
-          --xNextPos;
-        }
+        ( xDest < xPos ) ? planLeft() : calcAltDirection();
       }
       else if( yPos != yDest )
       {
-        if( yPos < yDest )
-        {
-          vehicleDir = DOWN;
-          ++yNextPos;
-        }
-        else
-        {
-          vehicleDir = UP;
-          --xNextPos;
-        }
+        ( yDest < yPos ) ? planUp() : planDown();
       }
-      else  /* xPos == xDest && yPos == yDest */
+      else /* xPos == xDest && yPos == yDest */
       {
         vehicleDir = NaN;
       }
       break;
-      
+
     case RIGHT:
       if( xPos != xDest )
       {
-        if( xPos < xDest )
-        {
-          vehicleDir = RIGHT;
-          ++xNextPos;
-        }
-        else
-        {
-          // Want to go left, but can't make U-turn
-          if( yPos < yDest && ( yPos + 1 ) < rowNum )
-          {
-            vehicleDir = UP;
-            ++yNextPos;
-          }
-          else if( yPos >= yDest && ( yPos - 1 ) >= 0 )
-          {
-            vehicleDir = DOWN;
-            ++yNextPos;
-          }
-          else  /* ( yPos + 1 ) > rowNum && ( yPos - 1 ) < 0 */
-          {
-            // Stop
-            vehicleDir = NaN;
-          }
-        }
+        ( xDest < xPos ) ? calcAltDirection() : planRight();
       }
       else if( yPos != yDest )
       {
-        if( yPos < yDest )
-        {
-          vehicleDir = DOWN;
-          ++yNextPos;
-        }
-        else
-        {
-          vehicleDir = UP;
-          --xNextPos;
-        }
+        ( yDest < yPos ) ? planUp() : planDown();
       }
-      else  /* xPos == xDest && yPos == yDest */
+      else /* xPos == xDest && yPos == yDest */
       {
         vehicleDir = NaN;
       }
@@ -286,24 +355,315 @@ void Vehicle::calculateNextLocation()
   }
 }
 
-Taxi::Taxi( int x, int y, int rowNum, int colNum, bool hasPkt )
-    : Vehicle( x, y, rowNum, colNum, hasPkt )
+/**
+ * @brief Calculates an alterative direction
+ *
+ * @details Determines an optimal alterative way to the destination ignoring the
+ *          possiblity of going fowards or backward
+ *
+ * @pre None
+ *
+ * @post Vehicle direction not forward or backward; next position is modified
+ *
+ * @par Algorithm
+ *      Find optimal 90 degree alternate direction
+ *      If no other direction possible, then direction set to NaN
+ *
+ * @exception None
+ *
+ * @param None
+ *
+ * @return Whether vehicle is boxed in on its sides
+ *
+ * @note If vehicle is boxed in on both sides left/right or up/down, then
+ *       stops vehicle
+ */
+bool Vehicle::calcAltDirection()
 {
-  setDestination();
+  bool canMove = false;
+
+  // Reset next position
+  xNextPos = xPos;
+  yNextPos = yPos;
+
+  // Want to go alternate direction, but can't make U-turn
+  switch( vehicleDir )
+  {
+    case NaN:
+      vehicleDir = NaN;
+      break;
+
+    case UP:
+    case DOWN:
+      canMove = ( xDest < xPos ) ? planLeft() : planRight();
+
+      if( !canMove ) /* can't move; Go in other direction */
+      {
+        canMove = ( xDest < xPos ) ? planRight() : planLeft();
+
+        if( !canMove ) stop(); /* boxed in */
+      }
+      break;
+
+    case LEFT:
+    case RIGHT:
+      canMove = ( yDest < yPos ) ? planUp() : planDown();
+
+      if( !canMove ) /* can't move; Go in other direction */
+      {
+        canMove = ( yDest < yPos ) ? planDown() : planUp();
+
+        if( !canMove ) stop(); /* boxed in */
+      }
+      break;
+  }
+
+  return canMove;
 }
 
-void Taxi::setDestination()
+/**
+ * @brief Stop vehicle
+ *
+ * @details Sets next position equal to current position and sets direction
+ *          to NaN
+ *
+ * @pre None
+ *
+ * @post Next location = current position; direction = NaN;
+ *
+ * @par Algorithm
+ *      Set vehicle direction = NaN
+ *      Set next position equal = current position
+ *
+ * @exception None
+ *
+ * @param None
+ *
+ * @return None
+ *
+ * @note None
+ */
+void Vehicle::stop()
+{
+  vehicleDir = NaN;
+  xNextPos   = xPos;
+  yNextPos   = yPos;
+}
+
+/**
+ * @brief Sets next location and direction to go up
+ *
+ * @details Increments X next position and sets direction up if
+ *          within boundaries
+ *
+ * @pre None
+ *
+ * @post Next location and direction are modified
+ *
+ * @par Algorithm
+ *      Checks if next position would be out of bounds
+ *      Alters vehicle direction and next position
+ *
+ * @exception None
+ *
+ * @param None
+ *
+ * @return Returns true if turn is not out of bounds; false otherwise
+ *
+ * @note None
+ */ bool
+Vehicle::planUp()
+{
+  if( yNextPos - 1 >= 0 )
+  {
+    vehicleDir = UP;
+    --yNextPos;
+
+    return true;
+  }
+
+  return false;
+}
+
+/**
+ * @brief Sets next location and direction to go down
+ *
+ * @details Increments X next position and sets direction down if
+ *          within boundaries
+ *
+ * @pre None
+ *
+ * @post Next location and direction are modified
+ *
+ * @par Algorithm
+ *      Checks if next position would be out of bounds
+ *      Alters vehicle direction and next position
+ *
+ * @exception None
+ *
+ * @param None
+ *
+ * @return Returns true if turn is not out of bounds; false otherwise
+ *
+ * @note None
+ */
+bool Vehicle::planDown()
+{
+  if( yNextPos + 1 < rowMax )
+  {
+    vehicleDir = DOWN;
+    ++yNextPos;
+
+    return true;
+  }
+
+  return false;
+}
+
+/**
+ * @brief Sets next location and direction to go left
+ *
+ * @details Decrements X next position and sets direction left if
+ *          within boundaries
+ *
+ * @pre None
+ *
+ * @post Next location and direction are modified
+ *
+ * @par Algorithm
+ *      Checks if next position would be out of bounds
+ *      Alters vehicle direction and next position
+ *
+ * @exception None
+ *
+ * @param None
+ *
+ * @return Returns true if turn is not out of bounds; false otherwise
+ *
+ * @note None
+ */
+bool Vehicle::planLeft()
+{
+  if( xNextPos - 1 >= 0 )
+  {
+    vehicleDir = LEFT;
+    --xNextPos;
+
+    return true;
+  }
+
+  return false;
+}
+
+/**
+ * @brief Sets next location and direction to go right
+ *
+ * @details Increments X next position and sets direction right if
+ *          within boundaries
+ *
+ * @pre None
+ *
+ * @post Next location and direction are modified
+ *
+ * @par Algorithm
+ *      Checks if next position would be out of bounds
+ *      Alters vehicle direction and next position
+ *
+ * @exception None
+ *
+ * @param None
+ *
+ * @return Returns true if turn is not out of bounds; false otherwise
+ *
+ * @note None
+ */
+bool Vehicle::planRight()
+{
+  if( xNextPos + 1 < colMax )
+  {
+    vehicleDir = RIGHT;
+    ++xNextPos;
+
+    return true;
+  }
+
+  return false;
+}
+
+/**
+ * @brief Constructor for Taxi class derived from abstrat Vehicle class
+ *
+ * @details Calculates destination and next location
+ *
+ * @pre None
+ *
+ * @post None
+ *
+ * @par Algorithm
+ *      Calculates destination
+ *      Calculates next location
+ *
+ * @exception None
+ *
+ * @param [in] x
+ *             X position of vehicle
+ *
+ *        [in] y
+ *             Y position of vehicle
+ *
+ *        [in] rowMax
+ *             Number of rows allowed
+ *
+ *        [in] colNum
+ *             Number of columns allowed
+ *
+ *        [in] hasPkt
+ *             Contains packet
+ *
+ * @note None
+ */
+Taxi::Taxi( int x, int y, int rowMax, int colNum, bool hasPkt )
+    : Vehicle( x, y, rowMax, colNum, hasPkt )
+{
+  calculateDestination();
+  calcNextLocation();
+}
+
+/**
+ * @brief Calculates Taxi's destination
+ *
+ * @details Randomly assigns destination within given row and column
+ *          boundaries using rand() generator
+ *
+ * @pre number of columns > 0
+ *      number of rows > 0
+ *
+ * @post New destination != old location
+ *
+ * @par Algorithm
+ *      Generate random xy coordinates until coordinates != old destination
+ *
+ * @exception None
+ *
+ * @param None
+ *
+ * @return None
+ *
+ * @note Recommend setting current location coordinates = old destination
+ *       before call
+ */
+void Taxi::calculateDestination()
 {
   int xNewDest, yNewDest;
-  
-  srand( time( NULL ) );
-  
+
   do
   {
-    xNewDest = rand() % ( columnNum - 1);
-    yNewDest = rand() % ( rowNum - 1);
-  }
-  while( !( yNewDest == yDest && xNewDest == xDest ) ); 
+    xNewDest = rand() % ( colMax - 1 );
+    yNewDest = rand() % ( rowMax - 1 );
+  } while( xNewDest == xDest && yNewDest == yDest );
+
+  xDest = xNewDest;
+  yDest = yNewDest;
 }
 
 // Terminating precompiler directives  ////////////////////////////////////////
